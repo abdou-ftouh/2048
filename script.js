7030880f1bd93ac3bf080411c8aa9b5b9b9953a4
abdou-ftouh <elftouh13@gmail.com>
2022-03-15 17:38:59 -0400

    combine the tiles with change colors and check if we have a winner

--- script.js
+++ script.js
@@ -1,158 +1,179 @@
 const square=document.querySelectorAll('.hidden')
 
+const myScore=document.getElementById('score')
+const win=document.getElementsByTagName('main')
 
-
+// Loop to remplair my squares
 for(let i=0;i<16;i++){
-    
         square[i].innerHTML=" "
        
-
 }
 
+// Simple winner function
+function winner(){
+  for(let i=0;i<16;i++){
+    if(square[i].innerHTML == 2048){
+    alert('you are the winner')
+      document.removeEventListener('keyup', myControl)
+     
+    }
+  }
+}
+
+// Colors for each Numbers
+function colours() {
+  for (let i=0; i < 16; i++) {
+   
+    if (square[i].innerHTML == " ") {
+      square[i].style.backgroundColor = 'white'
+    }
+    else if (square[i].innerHTML == 2) {
+      square[i].style.backgroundColor = 'green'
+    }
+      else if (square[i].innerHTML  == 4) {
+        square[i].style.backgroundColor = 'yellow' 
+      }
+      else if (square[i].innerHTML  == 8) {
+        square[i].style.backgroundColor = 'red' 
+      }
+      else if (square[i].innerHTML  == 16){ 
+        square[i].style.backgroundColor = 'blue'
+     }
+      else if (square[i].innerHTML  == 32) {
+        square[i].style.backgroundColor = '#d98243' 
+      }
+      else if (square[i].innerHTML  == 64) {
+        square[i].style.backgroundColor = '#00ffe7' 
+      }
+      else if (square[i].innerHTML == 128) {
+        square[i].style.backgroundColor = '#efc668' 
+      }
+      else if (square[i].innerHTML == 256) {
+        square[i].style.backgroundColor = '#79567f' 
+      }
+      else if (square[i].innerHTML == 512) {
+        square[i].style.backgroundColor = '#f8a78e' 
+      }
+      else if (square[i].innerHTML == 1024) {
+        square[i].style.backgroundColor = '#6233a8' 
+      }
+      else if (square[i].innerHTML == 2048) {
+        square[i].style.backgroundColor = '#ff97ba' 
+      }
+    
+  }
+}
+// Generate the Number 2 
+let score=0
 function generate(){
     let randomNumber=Math.floor(Math.random()*16)
     if(square[randomNumber].innerHTML==" "){
-        square[randomNumber].innerHTML=2
-        
+        square[randomNumber].innerHTML=2  
+         myScore.innerHTML=score
        
     }else generate()
+   
    colours()
 } 
 generate()
 generate()
 colours()
 
-function colours() {
-  for (let i=0; i < 16; i++) {
-   
-    if (square[i].innerHTML == " ") {square[i].style.backgroundColor = 'white'}
-    else if (square[i].innerHTML == 2) {square[i].style.backgroundColor = 'green'}
-      else if (square[i].innerHTML  == 4) {square[i].style.backgroundColor = 'yellow' }
-      else if (square[i].innerHTML  == 8) {square[i].style.backgroundColor = 'red' }
-      else if (square[i].innerHTML  == 16){ square[i].style.backgroundColor = 'blue' }
-      else if (square[i].innerHTML  == 32) {square[i].style.backgroundColor = '#d98243' }
-      else if (square[i].innerHTML  == 64) {square[i].style.backgroundColor = '#00ffe7' }
-      else if (square[i].innerHTML == 128) {square[i].style.backgroundColor = '#efc668' }
-      else if (square[i].innerHTML == 256) {square[i].style.backgroundColor = '#79567f' }
-      else if (square[i].innerHTML == 512) {square[i].style.backgroundColor = '#f8a78e' }
-      else if (square[i].innerHTML == 1024) {square[i].style.backgroundColor = '#6233a8' }
-      else if (square[i].innerHTML == 2048) {square[i].style.backgroundColor = '#ff97ba' }
-    
-  }
-}
+// Swipe Right
+
 function goRight() {
     for (let i=0; i < 16; i++) {
       if (i % 4 === 0) {
-        let firstElement = square[i].innerHTML
-        let secondElement = square[i+1].innerHTML
-        let therdElement = square[i+2].innerHTML
-        let fourthElement = square[i+3].innerHTML
-        let row = [firstElement, secondElement, therdElement, fourthElement]
-       
-        row=row.map(num=>Number(num))  //convert my string to number
-        console.log(row)
+        let myArray = [square[i].innerHTML, square[i+1].innerHTML, square[i+2].innerHTML, square[i+3].innerHTML]
+        myArray=myArray.map(num=>Number(num))  //convert my string to number
+        console.log(myArray)
         // go evreything if not a zero
-        let myFilter = row.filter(val => val)  //takes any value without zero
-       
+        let myFilter = myArray.filter(val => val)  //takes any value without zero
+    
         let missing = 4 - myFilter.length
         let zeros = Array(missing).fill(" ")
-        let newRow = zeros.concat(myFilter)
-  
-        square[i].innerHTML = newRow[0]
-        square[i +1].innerHTML = newRow[1]
-        square[i +2].innerHTML = newRow[2]
-        square[i +3].innerHTML = newRow[3]
+        let newArray = zeros.concat(myFilter)
+        
+        square[i].innerHTML = newArray[0]
+        square[i +1].innerHTML = newArray[1]
+        square[i +2].innerHTML = newArray[2]
+        square[i +3].innerHTML = newArray[3]
         
        
       }
     }
   }
 
-  
+  //Swipe left
   function goLeft() {
     for (let i=0; i < 16; i++) {
       if (i % 4 === 0) {
-        let firstElement = square[i].innerHTML
-        let secondElement = square[i+1].innerHTML
-        let therdElement = square[i+2].innerHTML
-        let fourthElement = square[i+3].innerHTML
-        let row = [firstElement, secondElement, therdElement, fourthElement]
-       
-        row=row.map(num=>Number(num))  //convert my string to number
-        console.log(row)
+        let myArray = [square[i].innerHTML, square[i+1].innerHTML, square[i+2].innerHTML, square[i+3].innerHTML]
+        myArray=myArray.map(num=>Number(num))  //convert my string to number
+        console.log(myArray)
         // go evreything if not a zero
-        let myFilter = row.filter(val => val)  //takes any value without zero
-       
+        let myFilter = myArray.filter(val => val)  //takes any value without zero
+    
         let missing = 4 - myFilter.length
         let zeros = Array(missing).fill(" ")
-        let newRow = myFilter.concat(zeros)
+        let newArray = myFilter.concat(zeros)
 
-        square[i].innerHTML = newRow[0]
-        square[i +1].innerHTML = newRow[1]
-        square[i +2].innerHTML = newRow[2]
-        square[i +3].innerHTML = newRow[3]
+        square[i].innerHTML = newArray[0]
+        square[i +1].innerHTML = newArray[1]
+        square[i +2].innerHTML = newArray[2]
+        square[i +3].innerHTML = newArray[3]
        
       }
     }
   }
-
+  //Swipe Up
   function goUp() {
     for (let i=0; i <4; i++) {
-      
-      let firstElement = square[i].innerHTML
-      let secondElement = square[i+4].innerHTML
-      let therdElement = square[i+8].innerHTML
-      let fourthElement = square[i+12].innerHTML
-      let row = [firstElement, secondElement, therdElement, fourthElement]
-      row=row.map(num=>Number(num))  //convert my string to number
+     
+      let myArray = [square[i].innerHTML, square[i+4].innerHTML, square[i+8].innerHTML, square[i+12].innerHTML]
+      myArray=myArray.map(num=>Number(num))  //convert my string to number
       // go evreything if not a zero
-      let myFilter = row.filter(val => val)  //takes any value without zero
+      let myFilter = myArray.filter(val => val)  //takes any value without zero
      
       let missing = 4 - myFilter.length
       let zeros = Array(missing).fill(" ")
-      let newRow = myFilter.concat(zeros)
-      // console.log(newRow)
-  
-     
-      square[i].innerHTML = newRow[0]
-      square[i +4].innerHTML = newRow[1]
-      square[i +8].innerHTML = newRow[2]
-      square[i +12].innerHTML = newRow[3]
+      let newArray = myFilter.concat(zeros)
+      // console.log(newArray)
+ 
+      square[i].innerHTML = newArray[0]
+      square[i +4].innerHTML = newArray[1]
+      square[i +8].innerHTML = newArray[2]
+      square[i +12].innerHTML = newArray[3]
     
        
       }
     }
 
-
+//Swipe Down
     function goDown() {
         for (let i=0; i <4; i++) {
           
-            let firstElement = square[i].innerHTML
-            let secondElement = square[i+4].innerHTML
-            let therdElement = square[i+8].innerHTML
-            let fourthElement = square[i+12].innerHTML
-            let row = [firstElement, secondElement, therdElement, fourthElement]
-            row=row.map(num=>Number(num))  //convert my string to number
-            // go evreything if not a zero
-            let myFilter = row.filter(val => val)  //takes any value without zero
-           
-            let missing = 4 - myFilter.length
-            let zeros = Array(missing).fill(" ")
-            let newRow = zeros.concat(myFilter)
-            console.log(newRow)
-
-           
-            square[i].innerHTML = newRow[0]
-            square[i +4].innerHTML = newRow[1]
-            square[i +8].innerHTML = newRow[2]
-            square[i +12].innerHTML = newRow[3]
+          let myArray = [square[i].innerHTML, square[i+4].innerHTML, square[i+8].innerHTML, square[i+12].innerHTML]
+          myArray=myArray.map(num=>Number(num))  //convert my string to number
+          // go evreything if not a zero
+          let myFilter = myArray.filter(val => val)  //takes any value without zero
+         
+          let missing = 4 - myFilter.length
+          let zeros = Array(missing).fill(" ")
+          let newArray = zeros.concat(myFilter)
+          // console.log(newmyArray)
+      
+          square[i].innerHTML = newArray[0]
+          square[i +4].innerHTML = newArray[1]
+          square[i +8].innerHTML = newArray[2]
+          square[i +12].innerHTML = newArray[3]
         
            
           }
         }
        
  
-
+ // My KeyPress Control 
 
 function myControl(e){
     if (e.key == "d"){
@@ -168,43 +189,82 @@ function myControl(e){
         keyUp()
     }
 }
-
+// KeyRight Control
 function keyRight(){
     goRight()
+    addNumber1()
+    goRight()
+    generate()
    
-
-    colours()
-   
-  
 }
+// KeyLeft Control
 function keyLeft(){
     goLeft()
-
- 
-    colours()
- 
+    addNumber1()
+    goLeft()
+    generate()
    
   
 }
+// KeyDown Control
 function keyDown(){
     goDown()
- 
-    colours()
- 
+    addNumber2()
+    goDown()
+    generate()
     
   
 }
+// KeyUp Control
 function keyUp(){
-    goUp  ()
-
-    colours()
-
+    goUp()
+    addNumber2()
+    goUp()
+    generate()
   
     
-  
+}
+// My Event 
+document.addEventListener('keyup',myControl)
+
+// combine my row numbers
+function addNumber1() {
+  for (let i =0; i < 15; i++) {
+       let a=square[i]
+       let b=square[i+1]
+    if (a.innerHTML === b.innerHTML) {
+      let total = Number(a.innerHTML) + Number(b.innerHTML)
+      a.innerHTML = total
+      b.innerHTML = 0
+      score=score+total;
+ 
+    }
+   
+  }
+  winner()
+}
+// combine my column numbers
+function addNumber2() {
+  for (let i =0; i < 12; i++) {
+    let a=square[i]
+    let b=square[i+4]
+    if (a.innerHTML === b.innerHTML) {
+      let total = Number(a.innerHTML) + Number(b.innerHTML)
+      a.innerHTML = 0
+      b.innerHTML = total
+     score=score+total;
+    }
+
+  }
+  winner()
+}
+// Reset My Game
+function gameReset(){
+  window.location.reload();
 }
 
-document.addEventListener('keyup',myControl)
+
+
 
 
 
